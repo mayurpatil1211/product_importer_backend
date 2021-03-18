0079bd32763a59bd9efdf5fe412a5b151df53cdd
mayurpatil1211 <mayur.patil1211@gmail.com>
2021-03-18 23:38:53 +0530

    added authentication to connect to postgress

--- app/tasks/webhook_task.py
+++ app/tasks/webhook_task.py
@@ -1,21 +1,29 @@
 from celery import shared_task
 from app.models import *
 from app.serializers import *
+import requests
 
 @shared_task
 def webhook_logging(event, object_dict):
 	links = WebhookLinks.objects.all()
 
 	for link in links:
-		serializer = WebhookEventsLogSerializer(
-				data={
+		data = {
 					'link' : link.link,
 					'event' : event,
 					'meta_data' : object_dict
 				}
+		serializer = WebhookEventsLogSerializer(
+				data=data
 			)
 		if serializer.is_valid():
 			serializer.save()
 		else:
 			print(serializer.errors)
+
+		try:
+			requests.post(link.link, data=data)
+		except(Exception)as e:
+			pass
+
 	return 'saved'
